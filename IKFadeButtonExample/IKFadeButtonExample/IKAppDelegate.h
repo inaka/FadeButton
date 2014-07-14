83fc907d95cfb10b06e12ea2009cb822194ba146
Pablo Villar <pablo@inakanetworks.com>
2014-07-14 13:06:16 -0300

    Added sample project.

---
+++ IKFadeButtonExample/IKFadeButtonExample/IKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IKAppDelegate.h
+//  IKFadeButtonExample
+//
+//  Created by Pablo Villar on 7/14/14.
+//  Copyright (c) 2014 Inaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
