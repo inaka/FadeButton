08780f4f11ae30e28a9b700bfbe29deb30e7c99f
Pablo Villar <pablo@inakanetworks.com>
2014-07-14 12:15:52 -0300

    Added IKFadeButton class.

---
+++ IKFadeButton/IKFadeButton.h
@@ -0,0 +1,15 @@
+//
+//  IKFadeButton.h
+//  Inaka
+//
+//  Created by Pablo Villar on 7/11/14.
+//  Copyright (c) 2014 Inaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKFadeButton : UIButton
+
+@property (readwrite, nonatomic) CGFloat fadeDuration;
+
+@end
